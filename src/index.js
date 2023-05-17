8239d5a1e36546e71a2eb0d5c90f92b8605569e1
Ricardo Otero <oterosantos@gmail.com>
2023-05-17 11:16:29 +0100

    Update files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("Hello from src/index.js!");
+const list = document.querySelector('#movie-list');
+const form = document.querySelector('#search-form');
+
+const insertMovies = (json) => {
+  list.innerHTML = ''; // remove the previous lis in the list
+
+  const movies = json.Search; // array of movies
+
+  movies.forEach((movie) => {
+    const liContent = `<li class="list-inline-item">
+      <img src="${movie.Poster}" />
+      <p>${movie.Title}</p>
+    </li>`;
+
+    list.insertAdjacentHTML('afterbegin', liContent);
+  });
+};
+
+// callback
+const handleSearch = (event) => {
+  event.preventDefault();
+
+  const input = document.querySelector('#search-input');
+  const url = `http://www.omdbapi.com/?s=${input.value}&apikey=adf1f2d7`;
+
+  fetch(url)
+    .then((data) => data.json())
+    .then((json) => insertMovies(json));
+};
+
+form.addEventListener('submit', handleSearch);
+
+fetch('http://www.omdbapi.com', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ Poster: 'http://gogoleco.com/image.png', Title: 'Portuguese Spiderman' }),
+});
